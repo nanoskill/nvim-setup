5dc85b948d140b1dcbf422c527806a93dca0c09c
Ravian <ravian.developer@gmail.com>
2025-06-17 23:19:40 +0700

    Autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -4,32 +4,11 @@ return {
     lazy = false,
     build = ":TSUpdate",
     branch = 'main',
-    opts = {
-      -- A list of parser names, or "all" (the five listed parsers should always be installed)
-      ensure_installed = { "c", "go", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html", "css", "python" },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-      },
-
-      autotag = {
-        enable = true,
-      }
-    },
-    config = function(_, opts) 
-      require("nvim-treesitter").setup(opts)
-      
-      local tsgroup = vim.api.nvim_create_augroup('treesitter', { clear = true})
+    config = function()
+      local tsgroup = vim.api.nvim_create_augroup('treesitter', { clear = true })
       vim.api.nvim_create_autocmd('FileType', {
-        group= tsgroup,
-        pattern = opts.ensure_installed,
+        group = tsgroup,
+        pattern = { "c", "go", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html", "css", "python" },
         callback = function() vim.treesitter.start() end,
       })
     end
